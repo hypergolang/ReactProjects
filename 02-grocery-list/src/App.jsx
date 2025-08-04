f12cced2f108dea53cefaae028a81e3c00449f61
hypergolang <hypergolang@gmail.com>
2025-08-04 22:18:03 +0700

    feat: Add basic layout and styling for the grocery list app

--- 02-grocery-list/src/App.jsx
+++ 02-grocery-list/src/App.jsx
@@ -1,10 +1,39 @@
 import './App.css';
+import groceryCartImg from './assets/grocery-cart.png';
 
 function App() {
     return (
-        <div className='App'>
-            <h1>Hello</h1>
-        </div>
+        <main className='App'>
+            <div>
+                <h4 className='success'>You're done</h4>
+                <div className='header'>
+                    <h1>Shopping List</h1>
+                    <img
+                        src={ groceryCartImg }
+                        alt=''
+                    />
+                    <input
+                        type='text'
+                        placeholder='Add an item'
+                    />
+                </div>
+                <ul>
+                    <li>
+                        <div className='container'>
+                            <input
+                                type='checkbox'
+                                name=''
+                                id=''
+                            />
+                            <p>Carrots</p>
+                        </div>
+                        <div>
+                            <button className='remove-button'>X</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </main>
     );
 }
 
