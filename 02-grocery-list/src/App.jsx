f427e7e6d7252fab5d0b34d159aa9e88e278bd00
hypergolang <hypergolang@gmail.com>
2025-08-05 12:31:25 +0700

    feat: Add grocery items to the list on pressing enter

--- 02-grocery-list/src/App.jsx
+++ 02-grocery-list/src/App.jsx
@@ -2,21 +2,49 @@ import { useState } from 'react';
 import './App.css';
 import groceryCartImg from './assets/grocery-cart.png';
 
+// {
+//     name: "Banana",
+//     quantity: 1,
+//     completed: true
+// }
+
 function App() {
-    const [inputValue, setInputValue] = useState("")
+    const [inputValue, setInputValue] = useState('');
+    const [groceryItems, setGroceryItems] = useState([]);
 
     const handleChangeInputValue = (e) => {
         setInputValue(e.target.value);
-    }
+    };
+
+    const handleAddGroceryItem = (e) => {
+        if (e.key === 'Enter') {
+            if (inputValue) {
+                setGroceryItems([
+                    ...groceryItems,
+                    {
+                        quantity: 1,
+                        name: inputValue,
+                        completed: false,
+                    },
+                ]);
+                setInputValue("")
+            }
+        }
+    };
 
     return (
         <main className='App'>
             <div>
                 <h4 className='success'>You're done</h4>
+                { JSON.stringify(groceryItems) }
                 <div className='header'>
-                    <button onClick={ () => {
-                        setInputValue("")
-                    } }>Clear input</button>
+                    <button
+                        onClick={ () => {
+                            setInputValue('');
+                        } }
+                    >
+                        Clear input
+                    </button>
                     <h1>Shopping List</h1>
                     <img
                         src={ groceryCartImg }
@@ -27,6 +55,7 @@ function App() {
                         placeholder='Add an item'
                         className='item-input'
                         onChange={ handleChangeInputValue }
+                        onKeyDown={ handleAddGroceryItem }
                         value={ inputValue }
                     />
                 </div>
