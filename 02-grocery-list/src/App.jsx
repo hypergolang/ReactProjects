fd32c9d3d6d4ca6010b99720a426e2e3547926a0
hypergolang <hypergolang@gmail.com>
2025-08-05 10:53:56 +0700

    feat: Implement input field and clear button

--- 02-grocery-list/src/App.jsx
+++ 02-grocery-list/src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import groceryCartImg from './assets/grocery-cart.png';
 
 function App() {
+    const [inputValue, setInputValue] = useState("")
+
+    const handleChangeInputValue = (e) => {
+        setInputValue(e.target.value);
+    }
+
     return (
         <main className='App'>
             <div>
                 <h4 className='success'>You're done</h4>
                 <div className='header'>
+                    <button onClick={ () => {
+                        setInputValue("")
+                    } }>Clear input</button>
                     <h1>Shopping List</h1>
                     <img
                         src={ groceryCartImg }
@@ -15,6 +25,9 @@ function App() {
                     <input
                         type='text'
                         placeholder='Add an item'
+                        className='item-input'
+                        onChange={ handleChangeInputValue }
+                        value={ inputValue }
                     />
                 </div>
                 <ul>
